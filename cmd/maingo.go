70f7dcbe75bd5cd4d2f2f041c015d33125ac9273
Oleg <gnomnano97@yandex.ru>
2025-10-05 15:15:57 +0300

    changes

--- cmd/maingo.go
+++ cmd/maingo.go
@@ -2,68 +2,205 @@ package main
 
 import (
 	"context"
-	"net/http"
-
-	"github.com/go-jose/go-jose/v4/json"
 	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"net/http"
+	"sync"
 )
 
-import in "order/pkg/pb/inventory/inventory"
-import pay "order/pkg/pb/payment/payment"
+import (
+	ap "order/api"
+	in "order/pkg/pb/inventory/inventory"
+	pay "order/pkg/pb/payment/payment"
+)
 
 type Order struct {
-	order_uuid       string   `json:"order_uuid"`
-	user_uuid        string   `json:"user_uuid"`
-	part_uuids       []string `json:"part_uuids"`
-	total_price      float64  `json:"total_price"`
-	transaction_uuid *string  `json:"transaction_uuid"`
-	payment_method   *string  `json:"payment_method"`
-	status           string   `json:"status"`
+	OrderUUID       string   `json:"order_uuid"`
+	UserUUID        string   `json:"user_uuid"`
+	PartsUUID       []string `json:"parts_uuid"`
+	TotalPrice      float64  `json:"total_price"`
+	TransactionUUID *string  `json:"transaction_uuid"`
+	PaymentMethod   *string  `json:"payment_method"`
+	Status          string   `json:"status"`
 }
 
-type PaymentMethod int
-
 const (
-	UNKNOWN PaymentMethod = iota
-	CARD
-	SBP
-	CREDIT_CARD
-	INVESTOR_MONEY
+	PaymentMethodUnknown = iota
+	PaymentMethodCARD
+	PaymentMethodSBP
+	PaymentMethodCREDITCARD
+	PaymentMethodINVESTORMONEY
 )
 
 var InventoryAddress = ":50052"
 var PaymentAddress = ":50051"
 
-func HandleCreateOrder(w http.ResponseWriter, r *http.Request, ctx context.Context, client in.InventoryServiceClient) (string, float64, error) {
-	var order Order
-	err := json.NewDecoder(r.Body).Decode(&order)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
+type OrderStorage struct {
+	mu     sync.RWMutex
+	Orders map[string]*Order
+}
 
-	req := &in.ListPartsRequest{
+func NewOrderStorage() *OrderStorage {
+	return &OrderStorage{
+		Orders: make(map[string]*Order),
+	}
+}
+
+type OrderHandler struct {
+	orders    *OrderStorage
+	inventory in.InventoryServiceClient
+	payment   pay.PaymentClient
+}
+
+func NewOrderHandler(order *OrderStorage, inv in.InventoryServiceClient, pay pay.PaymentClient) *OrderHandler {
+	return &OrderHandler{orders: order, inventory: inv, payment: pay}
+}
+
+type Server struct {
+	in.InventoryServiceClient
+	pay.PaymentClient
+}
+
+func PaymToEnum(s string) (pay.PaymentMethod, error) {
+	switch s {
+	case "CARD":
+		return pay.PaymentMethod_PAYMENT_METHOD_CARD, nil
+	case "SBP":
+		return pay.PaymentMethod_PAYMENT_METHOD_SBP, nil
+	case "CREDITCARD":
+		return pay.PaymentMethod_PAYMENT_METHOD_CARD, nil
+	case "INVESTORMONEY":
+		return pay.PaymentMethod_PAYMENT_METHOD_INVESTOR_MONEY, nil
+	default:
+		return pay.PaymentMethod_PAYMENT_METHOD_UNKNOWN, nil
+	}
+}
+func (s *OrderHandler) CreateOrders(ctx context.Context, reqBody ap.CreateOrderRequest, server Server) (*ap.CreateOrderResponse, error) {
+
+	grpcToIn := &in.ListPartsRequest{
 		Filter: &in.PartsFilter{
-			Names: order.part_uuids,
+			Uuids: reqBody.PartUuids,
 		},
 	}
 
-	resp, err := client.ListParts(ctx, req)
+	grpcFromIn, err := server.ListParts(ctx, grpcToIn)
 	if err != nil {
-		status.Errorf(codes.InvalidArgument, "Something went wrong")
+		return nil, status.Error(codes.Internal, err.Error())
 	}
-	var total_price float64
-	for _, part := range resp.GetParts() {
-		if part == nil {
-			return "", 0, status.Errorf(codes.NotFound, "one of your parts is unavailible")
-		}
-		total_price += part.Price
-	}
-	order.order_uuid = uuid.New().String()
-	order.status = "PENDING_PAYMENT"
 
+	var TotalPrice float64
+
+	for _, part := range grpcFromIn.Parts {
+		reqToGetPart := &in.GetPartRequest{Uuid: part.UUID}
+		_, err1 := server.GetPart(ctx, reqToGetPart)
+		if err1 != nil {
+			return nil, status.Error(codes.Internal, err1.Error())
+		}
+		TotalPrice += part.Price
+	}
+	UUID := uuid.New()
+	order := &Order{
+		OrderUUID:  UUID.String(),
+		UserUUID:   reqBody.UserUUID.String(),
+		PartsUUID:  reqBody.PartUuids,
+		TotalPrice: TotalPrice,
+		Status:     "PENDING_PAYMENT",
+	}
+
+	s.orders.Orders[order.OrderUUID] = order
+
+	return &ap.CreateOrderResponse{
+		OrderUUID:  UUID,
+		TotalPrice: TotalPrice,
+	}, nil
+}
+
+func (s *OrderHandler) PayOrder(ctx context.Context, res ap.PayOrderRequest, params ap.HandlePayOrderParams, server Server) (*ap.PayOrderResponse, error) {
+
+	ord, ok := s.orders.Orders[params.OrderUUID.String()]
+	if !ok {
+		return nil, status.Error(codes.NotFound, "order not found")
+	}
+
+	pm, err := PaymToEnum(res.PaymentMethod)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	grpcToPay := &pay.PayOrderRequest{
+		Order: &pay.OrderRequest{
+			OrderUuid:     ord.OrderUUID,
+			UserUuid:      ord.UserUUID,
+			PaymentMethod: pm,
+		},
+	}
+	grpcFromPay, err := server.PayOrder(ctx, grpcToPay)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	s.orders.Orders[params.OrderUUID.String()].Status = "PAID"
+	s.orders.Orders[params.OrderUUID.String()].TransactionUUID = &grpcFromPay.TransactionUuid
+
+	transUUID, err := uuid.Parse(grpcFromPay.TransactionUuid)
+	if err != nil {
+		return nil, status.Error(codes.Canceled, err.Error())
+	}
+
+	return &ap.PayOrderResponse{
+		TransactionUUID: transUUID,
+	}, nil
+}
+
+func (s *OrderHandler) GetOrder(params ap.HandleGetOrderParams, server Server) (*ap.GetOrderResponse, error) {
+	ord, ok := s.orders.Orders[params.OrderUUID.String()]
+	if !ok {
+		return nil, status.Error(codes.NotFound, "order not found")
+	}
+
+	parsedUserUUID, err := uuid.Parse(ord.UserUUID)
+	transUUID, err := uuid.Parse(*ord.TransactionUUID)
+	if err != nil {
+		return nil, status.Error(codes.Canceled, err.Error())
+	}
+
+	finalTransUUID := ap.OptNilUUID{Value: transUUID, Set: true, Null: false}
+	finalPaymentMethod := ap.OptNilString{Value: *ord.PaymentMethod, Set: true, Null: false}
+
+	return &ap.GetOrderResponse{
+		OrderUUID:       params.OrderUUID,
+		UserUUID:        parsedUserUUID,
+		PartUuids:       ord.PartsUUID,
+		TotalPrice:      ord.TotalPrice,
+		TransactionUUID: finalTransUUID,
+		PaymentMethod:   finalPaymentMethod,
+		Status:          ord.Status,
+	}, nil
+}
+
+func (s *OrderHandler) CancelOrder(params ap.HandleCancelOrderParams) ap.Error {
+
+	ord, ok := s.orders.Orders[params.OrderUUID.String()]
+	if !ok {
+		return ap.Error{Code: "404", Message: "order not found"}
+	}
+	if ord.Status == "PAID" {
+		return ap.Error{
+			Code:    "409",
+			Message: "order already paid",
+		}
+	}
+	if ord.Status == "PENDING_PAYMENT" {
+		ord.Status = "CANCELLED"
+		return ap.Error{
+			Code:    "204",
+			Message: "No content",
+		}
+	}
+	return ap.Error{}
 }
 func main() {
+	orders := NewOrderStorage()
 
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,36 @@ module order
 go 1.25.1
 
 require (
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
-	google.golang.org/grpc v1.75.1 // indirect
-	google.golang.org/protobuf v1.36.6 // indirect
+	github.com/go-faster/errors v0.7.1
+	github.com/go-faster/jx v1.1.0
+	github.com/go-jose/go-jose/v4 v4.1.3
+	github.com/google/uuid v1.6.0
+	github.com/ogen-go/ogen v1.15.2
+	go.opentelemetry.io/otel v1.38.0
+	go.opentelemetry.io/otel/metric v1.38.0
+	go.opentelemetry.io/otel/trace v1.38.0
+	google.golang.org/grpc v1.75.1
+	google.golang.org/protobuf v1.36.10
+)
+
+require (
+	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-faster/yaml v0.4.6 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/segmentio/asm v1.2.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp v0.0.0-20251002181428-27f1f14c8bb9 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251002232023-7c0ddcbb5797 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
